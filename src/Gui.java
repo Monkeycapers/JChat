62d91836b28b1ef45f7045bdbe782ddf56e6ad87
Evan <evanjesty@gmail.com>
2016-08-21 20:42:01 -0400

    Added a /connect method

--- src/Gui.java
+++ src/Gui.java
@@ -28,6 +28,8 @@ public class Gui {
     String title;
 
     boolean pendingMessage = false;
+    public static boolean readyToConnect = false;
+    public static boolean readyToDisconnect = false;
     String message = "Test";
     String nick = "Anon";
 
@@ -59,13 +61,16 @@ public class Gui {
             {
 
                 message = textField.getText();
-                if (message.startsWith("/nick")) {
+                if (message.toLowerCase().startsWith("/nick")) {
                     try {
                         nick = message.substring(6);
                     }
                     catch (Exception ex) {
                         addText("Could not change nickname.");
                     }
+                }
+                else if (message.toLowerCase().startsWith("/connect")) {
+                    readyToConnect = true;
                 }
                 else {
                     pendingMessage = true;
@@ -85,7 +90,7 @@ public class Gui {
         container.setResizable(true);
 
 
-
+        //TODO: Grab from settings
         client = new Client("localhost", 16000);
         client.run();
 
@@ -107,67 +112,75 @@ public class Gui {
             this.portNumber = portNumber;
         }
         public void run() {
-            addText("Starting JChat internal server...");
             while (true) {
-                //Get a Socket connection to the host
-                try (
-                        Socket echoSocket = new Socket(hostName, portNumber);     //new InputStreamReader(System.in))
-                        OutputStream outToServer = echoSocket.getOutputStream();
-                        DataOutputStream out = new DataOutputStream(outToServer);
-                        InputStream inFromServer = echoSocket.getInputStream();
-                        DataInputStream in = new DataInputStream(inFromServer);
-                ) {
-                    //TODO: Verify if the server is a JChat server
-                    addText("Connected to a JChat server on " + hostName + ":" + portNumber);
 
-                    String line = "";
-                    boolean running = true;
-                    while (running) {
-                        try {
-                            String oldLine = line;
-                            line = in.readUTF();
-                            if (!(oldLine.equals(line))) {
-                                addText(line);
+                if (readyToConnect) {
+                    //Get a Socket connection to the host
+                    try (
+                            Socket echoSocket = new Socket(hostName, portNumber);     //new InputStreamReader(System.in))
+                            OutputStream outToServer = echoSocket.getOutputStream();
+                            DataOutputStream out = new DataOutputStream(outToServer);
+                            InputStream inFromServer = echoSocket.getInputStream();
+                            DataInputStream in = new DataInputStream(inFromServer);
+                    ) {
+                        //TODO: Verify if the server is a JChat server
+                        addText("Connected to a JChat server on " + hostName + ":" + portNumber);
+
+                        String line = "";
+                        boolean running = true;
+                        while (running) {
+                            try {
+                                String oldLine = line;
+                                line = in.readUTF();
+                                if (!(oldLine.equals(line))) {
+                                    addText(line);
+                                }
+
+                            } catch (IOException e) {
+                                System.out.println("Could not receive a message");
+                                running = false;
+                            }
+
+                            try {
+                                if (pendingMessage) {
+                                    System.out.println("sent a message");
+                                    pendingMessage = false;
+                                    out.writeUTF("<" + nick + ">  " + message);
+                                } else {
+                                    out.writeUTF(nick + ",Alive");
+                                }
+                            } catch (IOException e) {
+                                System.out.println("Could not send a message");
+                                running = false;
+                            }
+
+                            try {
+                                Thread.sleep(1000);
+                            } catch (Exception ex) {
                             }
 
                         }
-                        catch (IOException e) {
-                            System.out.println("Could not receive a message");
-                            running = false;
-                        }
 
-                        try {
-                            if (pendingMessage) {
-                                System.out.println("sent a message");
-                                pendingMessage = false;
-                                out.writeUTF("<" + nick + ">  " + message);
-                            }
-                            else {
-                                out.writeUTF(nick + ",Alive");
-                            }
-                        }
-                        catch (IOException e) {
-                            System.out.println("Could not send a message");
-                            running = false;
-                        }
-
-                        try {Thread.sleep(1000); } catch (Exception ex) { }
-
+                        //
+                        //Error in connecting to the host
+                    } catch (UnknownHostException e) {
+                        addText("Don't know about host " + hostName);
+                        System.exit(-1);
+                    } catch (IOException e) {
+                        //This can happen if the server has an error or closes
+                        addText("Couldn't get I/O for the connection to " + hostName);
+                    } catch (Exception e) {
+                        addText("Unknown error");
+                        System.exit(-1);
                     }
 
-                    //
-                    //Error in connecting to the host
-                } catch (UnknownHostException e) {
-                    addText("Don't know about host " + hostName);
-                    System.exit(-1);
-                } catch (IOException e) {
-                    //This can happen if the server has an error or closes
-                    addText("Couldn't get I/O for the connection to " + hostName);
-                } catch (Exception e) {
-                    addText("Unknown error");
-                    System.exit(-1);
                 }
-
+                else {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception ex) {
+                    }
+                }
             }
 
             //
